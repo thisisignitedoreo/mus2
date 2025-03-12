e8858ec9c2997c4418798df69b9501d06100caa5
acid <oreoigniter@gmail.com>
2025-03-12 15:40:41 +0300

    Whoops

--- build.sh
+++ build.sh
@@ -2,11 +2,15 @@
 
 set -e
 
-CC="${1}gcc"
-FLAGS="-Wall -Wextra -Werror -g -std=gnu99 -I./ext/raylib/include -L./ext/raylib/lib -I./ext/strap/src -L./ext/strap"
-LIBS="-l:libraylib.a -lm -lpthread -ldl -lrt -lstrap"
-
-if test ! -f ext/strap/libstrap.a; then
+if [[ "t${1}" == "twindows" ]]; then
+    CC="x86_64-w64-mingw32-gcc"
+    FLAGS="-Wall -Wextra -Werror -g -std=gnu99 -I./ext/raylib-w/include -L./ext/raylib-w/lib -I./ext/strap/src -L./ext/strap"
+    LIBS="-l:libraylib.a -lgdi32 -lwinmm -lstrap"
+    ext/strap/build.sh && cd .
+else
+    CC="gcc"
+    FLAGS="-Wall -Wextra -Werror -g -std=gnu99 -I./ext/raylib/include -L./ext/raylib/lib -I./ext/strap/src -L./ext/strap"
+    LIBS="-l:libraylib.a -lm -lpthread -ldl -lrt -lstrap"
     ext/strap/build.sh && cd .
 fi
 
