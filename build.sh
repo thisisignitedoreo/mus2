5bcf8b224fd8f3727a7432af3b0df87e58ee2dbb
acid <oreoigniter@gmail.com>
2025-03-12 15:53:06 +0300

    Change build system a bit

--- build.sh
+++ build.sh
@@ -2,16 +2,27 @@
 
 set -e
 
+if test ! -f "src/assets.h"; then
+    echo "Bundling assets..."
+    python bundle.py
+fi
+
+echo "Building libstrap..."
+ext/strap/build.sh && cd .
+
+echo "Building mus2..."
 if [[ "t${1}" == "twindows" ]]; then
     CC="x86_64-w64-mingw32-gcc"
     FLAGS="-Wall -Wextra -Werror -g -std=gnu99 -I./ext/raylib-w/include -L./ext/raylib-w/lib -I./ext/strap/src -L./ext/strap"
     LIBS="-l:libraylib.a -lgdi32 -lwinmm -lstrap"
-    ext/strap/build.sh && cd .
+elif [[ "t${1}" == "twindows-release" ]]; then
+    CC="x86_64-w64-mingw32-gcc"
+    FLAGS="-Wall -Wextra -Werror -g -mwindows -std=gnu99 -I./ext/raylib-w/include -L./ext/raylib-w/lib -I./ext/strap/src -L./ext/strap"
+    LIBS="-l:libraylib.a -lgdi32 -lwinmm -lstrap"
 else
     CC="gcc"
     FLAGS="-Wall -Wextra -Werror -g -std=gnu99 -I./ext/raylib/include -L./ext/raylib/lib -I./ext/strap/src -L./ext/strap"
     LIBS="-l:libraylib.a -lm -lpthread -ldl -lrt -lstrap"
-    ext/strap/build.sh && cd .
 fi
 
 $CC $FLAGS -o mus2 src/main.c $LIBS
