b97ab66cf9f88af08dc0364221ef03f0c3f92f71
acid <oreoigniter@gmail.com>
2025-03-28 17:25:23 +0300

    Stop suggesting fixes for bundlers, go look at the code already

--- bundle.py
+++ bundle.py
@@ -1,9 +1,6 @@
 
 import os
 
-output_file = """
-// Created with bundle.py"""
-
 def replace_all(string, allowed, replacement):
     res = ""
     for i in string:
@@ -12,17 +9,20 @@ def replace_all(string, allowed, replacement):
     return res
 
 if __name__ == "__main__":
-    if not os.path.isdir("assets"): exit(1)
-    files = os.listdir("assets")
-    output_file += "\n// Bundled files: " + ", ".join(files) + "\n\n"
-    for i in files:
-        print(f"Bundling {i}")
-        name = replace_all(i.upper(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "_")
-        f = open(f"assets/{i}", "rb").read()
-        output_file += f"const unsigned long _{name}_LENGTH = {len(f)};\n"
-        output_file += f"char _{name}[] = " + "{ "
-        for b in f:
-            output_file += f"0x{hex(b)[2:].upper():0>2}, "
-        output_file += "};\n\n"
-
-    open("src/assets.h", "w").write(output_file)
+    with open("src/assets.h", "w") as f:
+        f.write("\n// Created with bundle.py")
+        if not os.path.isdir("assets"): exit(1)
+        files = os.listdir("assets")
+        f.write("\n// Bundled files: " + ", ".join(files) + "\n\n")
+        for i in files:
+            print(f"Bundling {i}")
+            name = replace_all(i.upper(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "_")
+            with open(f"assets/{i}", "rb") as a:
+                a.seek(0, 2)
+                f.write(f"const unsigned long _{name}_LENGTH = {a.tell()};\n")
+                a.seek(0, 0)
+                f.write(f"char _{name}[] = " + "{ ")
+                for b in a.read():
+                    f.write(f"0x{hex(b)[2:].upper():0>2}, ")
+                f.write("};\n\n")
+                
